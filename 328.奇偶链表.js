0bc5717d651b076d9be5226adce79dad11522754
张泽岳 <zhangzeyue@aixuexi.com>
2020-11-13 18:33:46 +0800

    update

---
+++ 328.奇偶链表.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=javascript
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+  if(head === null) return head
+  let evenHead = head.next;
+  let odd = head, even = evenHead;
+  while(even !== null && even.next !== null) {
+    odd.next = even.next
+    odd = odd.next
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead
+  return head
+};
+// @lc code=end
+
