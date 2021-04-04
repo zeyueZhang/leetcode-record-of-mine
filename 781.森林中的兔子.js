5525d5d969e399333d30186df0c3afa111acc336
张泽岳 <zhangzeyue@aixuexi.com>
2021-04-04 13:51:31 +0800

    781

---
+++ 781.森林中的兔子.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=781 lang=javascript
+ *
+ * [781] 森林中的兔子
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} answers
+ * @return {number}
+ */
+var numRabbits = function(answers) {
+  const count = new Map();
+  for (const y of answers) {
+  }
+  let ans = 0;
+  for (const [y, x] of count.entries()) {
+      ans += Math.floor((x + y) / (y + 1)) * (y + 1);
+  }
+  return ans;
+};
+// @lc code=end
+
